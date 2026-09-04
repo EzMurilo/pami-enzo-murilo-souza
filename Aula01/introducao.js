267ebe91b256af5c0c447f7ec92828abc3529db8
EzMurilo <enzomurilo321@gmail.com>
2026-09-03 22:04:53 -0300

    Cria arquivo que demonstra o uso de variaveis e concatenação

---
+++ Aula01/introducao.js
@@ -0,0 +1,29 @@
+console.log("Fala seus otário");
+
+//criação de variaveis
+var Variavel01 = "Oi, ";
+let Variavel02 = "Tudo Bem?";
+const Variavel03 = "Vocês são lindos";
+
+//concatenação de variaveis
+console.log(Variavel01 + Variavel02);
+console.log(Variavel01, Variavel02);
+
+Variavel01 = "Olá, ";
+console.log(Variavel01, Variavel02);
+
+let Teste = "Texto";
+console.log(typeof(Teste));
+Teste = 2;
+console.log(typeof(Teste));
+
+Teste = {
+    nome: "João",
+    idade: 38
+}
+
+console.log("O tipo agora é: ",typeof(Teste));
+
+Teste = [1, 2];
+
+console.log("O resultado do tipo de array é", typeof(Teste))
